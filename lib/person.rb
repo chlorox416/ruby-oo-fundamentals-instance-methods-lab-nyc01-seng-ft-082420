7383bbc3144ac0cddb9b34bc93141e3d5fd9356e
chlorox416 <chloechang416@gmail.com>
2020-08-30 23:11:19 -0400

    done

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+    def walk
+        puts "The Person is walking"
+    end
+
+    def talk
+        puts "Hello World!"
+    end
+
+    def walk
+        puts "The Person is walking"
+    end
+end
+
+ada = Person.new
+ada.talk
+ada.walk
